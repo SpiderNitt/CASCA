5027c6d9e94e662e87528ef167a2be1db155d60e
SANTHOSH17-DOT <santhoshm12052003@gmail.com>
2023-11-29 17:49:09 +0530

    Setup the docker containers

--- PACKET_INTERCEPTION/main.py
+++ PACKET_INTERCEPTION/main.py
@@ -3,24 +3,39 @@ from netfilterqueue import NetfilterQueue
 import socket
 import sys
 
-def process_packet(packet):
+def compress_packet(packet):
     scapy_packet = IP(packet.get_payload())
     if scapy_packet.haslayer(Raw):
         print(scapy_packet[TCP].payload)
         # coflow schedule and compression
     packet.accept()
 
-QUEUE_NUM = 1
+def decompress_packet(packet):
+    scapy_packet = IP(packet.get_payload())
+    if scapy_packet.haslayer(Raw):
+        print(scapy_packet[TCP].payload)
+        # coflow schedule and compression
+    packet.accept()
 
-print('Listening on NFQUEUE queue-num {}'.format(QUEUE_NUM))
+COMPRESS_QUEUE_NUM = 1
+DECOMPRESS_QUEUE_NUM = 2
 
-nfqueue = NetfilterQueue()
-nfqueue.bind(QUEUE_NUM, process_packet)
-s = socket.fromfd(nfqueue.get_fd(), socket.AF_UNIX, socket.SOCK_STREAM)
+print('Listening on NFQUEUE queue-num {} for compression'.format(COMPRESS_QUEUE_NUM))
+print('Listening on NFQUEUE queue-num {} for decompression'.format(DECOMPRESS_QUEUE_NUM))
+
+nfqueue1 = NetfilterQueue()
+nfqueue2 = NetfilterQueue()
+nfqueue1.bind(COMPRESS_QUEUE_NUM, compress_packet)
+nfqueue2.bind(DECOMPRESS_QUEUE_NUM, decompress_packet)
+s1 = socket.fromfd(nfqueue1.get_fd(), socket.AF_UNIX, socket.SOCK_STREAM)
+s2 = socket.fromfd(nfqueue2.get_fd(), socket.AF_UNIX, socket.SOCK_STREAM)
 try:
-    nfqueue.run_socket(s)
+    nfqueue1.run_socket(s1)
+    nfqueue2.run_socket(s2)
 except KeyboardInterrupt:
     sys.stdout.write('Exiting \n')
 
-s.close()
-nfqueue.unbind()
+s1.close()
+s2.close()
+nfqueue1.unbind()
+nfqueue2.unbind()
