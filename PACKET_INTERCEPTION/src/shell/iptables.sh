247464b40fc85f60b0c98b5b27ac765f2599d2c3
SANTHOSH17-DOT <santhoshm12052003@gmail.com>
2023-12-10 17:26:40 +0530

    Buggy double queue interception script

--- PACKET_INTERCEPTION/src/shell/iptables.sh
+++ PACKET_INTERCEPTION/src/shell/iptables.sh
@@ -1,4 +1,5 @@
 # Route the packets to netfilterqueue
-iptables -t raw -A PREROUTING -p tcp --source-port 80 -j NFQUEUE --queue-num 1
-iptables -t raw -A OUTPUT -p tcp --destination-port 80 -j NFQUEUE --queue-num 2
+source .env
+iptables -t raw -A PREROUTING -p tcp --source-port 80 -j NFQUEUE --queue-num $QUEUE_NUM
+iptables -t raw -A OUTPUT -p tcp --destination-port 80 -j NFQUEUE --queue-num $((QUEUE_NUM+1))
 echo "Configured IP tables successfully"
