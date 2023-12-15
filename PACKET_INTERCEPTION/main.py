7710bb74d32b8a05764307ce31d080b689772af1
SANTHOSH17-DOT <santhoshm12052003@gmail.com>
2023-12-15 22:07:51 +0530

    Remove redundant code

--- PACKET_INTERCEPTION/main.py
+++ PACKET_INTERCEPTION/main.py
@@ -1,9 +1,9 @@
 from scapy.all import *
 from netfilterqueue import NetfilterQueue
-import socket
-import sys
 import netifaces
 import zstandard as zstd
+from dotenv import load_dotenv
+load_dotenv()
 
 # Important stuffs about zstandard package
 # write_content_size parameter improves performance
@@ -43,19 +43,7 @@ for interface in interfaces:
     if 2 in addrs.keys():
         host_ips.append(addrs[netifaces.AF_INET][0]['addr'])
 
-def decompress_packet(packet):
-    print(packet)
-    scapy_packet = IP(packet.get_payload())
-    if scapy_packet.haslayer(Raw):
-        print(scapy_packet[TCP].payload)
-        # coflow schedule and decompression
-    packet.accept()
-
-COMPRESS_QUEUE_NUM = int(os.environ.get("QUEUE_NUM"))
-DECOMPRESS_QUEUE_NUM = int(os.environ.get("QUEUE_NUM"))+1
-
-print('Listening on NFQUEUE queue-num {} for compression'.format(COMPRESS_QUEUE_NUM))
-print('Listening on NFQUEUE queue-num {} for decompression'.format(DECOMPRESS_QUEUE_NUM))
+QUEUE_NUM = int(os.environ.get("QUEUE_NUM"))
 
 cctx = zstd.ZstdCompressor()
 dctx = zstd.ZstdDecompressor()
