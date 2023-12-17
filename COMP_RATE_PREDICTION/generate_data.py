6c61dc210959d50225cd7e4d254bf1b052a06765
SANTHOSH17-DOT <santhoshm12052003@gmail.com>
2023-12-17 12:17:25 +0530

    Inject stress on every 10 iterations

--- COMP_RATE_PREDICTION/generate_data.py
+++ COMP_RATE_PREDICTION/generate_data.py
@@ -26,11 +26,10 @@ with open(output_file,'a',newline="") as csvfile:
 
     for c_i in range(0,100,5): # CPU limit 0-100%
         for m_i in range(0,math.ceil(memory/1024)-1,1): # Memory usage 0-memory
-            injector.CPUStress(limit=c_i) # Inject CPU stress
-            if m_i>0:
-                injector.MemoryStress(gigabytes=m_i) # Inject memory stress
-            
             for i in range(10):
+                injector.CPUStress(limit=c_i) # Inject CPU stress
+                if m_i>0:
+                    injector.MemoryStress(gigabytes=m_i) # Inject memory stress
                 cpu_util = psutil.cpu_percent()
                 mem_util = psutil.virtual_memory().percent
                 cpu_freq = psutil.cpu_freq().current # Get CPU frequency in Mhz
@@ -51,7 +50,8 @@ with open(output_file,'a',newline="") as csvfile:
 
                 compressed_data_size += len(compressed_data)
                 compression_speed = total_data_size / compression_time / (1024*1024) #MB/s
-                compression_ratio = total_data_size / compressed_data_size;packet_size_diff_rate = (total_data_size - compressed_data_size) / compression_time / (1024*1024);
+                compression_ratio = total_data_size / compressed_data_size
+                packet_size_diff_rate = (total_data_size - compressed_data_size) / compression_time / (1024*1024)
 
                 print(time.strftime("%Y-%m-%d %H:%M:%S"), cpus, cpu_util, cpu_freq, memory, mem_util, compression_speed, compression_ratio, packet_size_diff_rate)
                 writer.writerow([time.strftime("%Y-%m-%d %H:%M:%S"), cpus, cpu_util, cpu_freq, memory, mem_util, compression_speed, compression_ratio, packet_size_diff_rate])
