56bc6aa2cb901867858807cddc94e2b8cdca52b4
SANTHOSH17-DOT <santhoshm12052003@gmail.com>
2023-12-17 08:52:39 +0530

    Update data generation script for controlled CPU utilization

---
+++ COMP_RATE_PREDICTION/README.md
@@ -0,0 +1,2 @@
+Enable venv
+Update COMP_RATE_PREDICTION/lib/python3.9/site-packages/stressinjector/cpu.py with COMP_RATE_PREDICTION/update_cpu.py
--- COMP_RATE_PREDICTION/generate_data.py
+++ COMP_RATE_PREDICTION/generate_data.py
@@ -6,6 +6,7 @@ import os
 import sys
 import stressinjector as injector
 import resource
+import math
 
 output_file = 'system_stats.csv'
 path = sys.argv[1]
@@ -24,16 +25,14 @@ with open(output_file,'w',newline="") as csvfile:
 # No of processes?
 
     for c_i in range(0,100,5): # CPU limit 0-100%
-        for m_i in range(0,memory,512): # Memory usage 0-memory
-            injector.CPUStress(10) # Inject cpu stress for 10 seconds
-            # Set CPU limit (Pending)
-            # Set memory limit
-            resource.setrlimit(resource.RLIMIT_AS, (m_i, m_i))
-
-            injector.MemoryStress(gigabytes=m_i/1024) # Inject memory stress
-            mem_util = psutil.virtual_memory().percent
-            cpu_util = psutil.cpu_percent()
+        for m_i in range(0,math.ceil(memory/1024)-1,1): # Memory usage 0-memory
+            injector.CPUStress(limit=c_i) # Inject CPU stress
+            if m_i>0:
+                injector.MemoryStress(gigabytes=m_i) # Inject memory stress
+            
             for i in range(10):
+                cpu_util = psutil.cpu_percent()
+                mem_util = psutil.virtual_memory().percent
                 cpu_freq = psutil.cpu_freq().current # Get CPU frequency in Mhz
 
                 # Initialize variables for compression
@@ -53,8 +52,8 @@ with open(output_file,'w',newline="") as csvfile:
                 compressed_data_size += len(compressed_data)
                 compression_speed = total_data_size / compression_time / (1024*1024) #MB/s
                 compression_ratio = total_data_size / compressed_data_size
-
+                print(time.strftime("%Y-%m-%d %H:%M:%S"), cpus, cpu_util, cpu_freq, memory, mem_util, compression_speed, compression_ratio)
                 writer.writerow([time.strftime("%Y-%m-%d %H:%M:%S"), cpus, cpu_util, cpu_freq, memory, mem_util, compression_speed, compression_ratio])
-                time.sleep(1)
+                time.sleep(0.1)
             
             print(f"System statistics recorded to: {output_file}")
