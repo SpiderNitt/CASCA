7edfbbb8eb50493c0f56d17d8b15e09562b770dc
SANTHOSH17-DOT <root@localhost.localdomain>
2023-12-19 16:35:40 +0530

    Remove src/shell

--- PACKET_INTERCEPTION/src/shell/ramdisk.sh
+++
@@ -1,5 +0,0 @@
-# Creating a mountable disk
-echo "Making directory for RAM Disk"
-sudo mkdir /mnt/pktramdisk
-echo "Mounting /mnt/pktramdisk"
-sudo mount -t tmpfs -o rw,size=256M tmpfs /mnt/pktramdisk
