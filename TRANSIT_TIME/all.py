f7bb7eb1afbf019ac8c49b766c5204c30a52a4a2
Naveen Nair <naveensuresh.nair@gmail.com>
2023-12-19 06:46:16 +0000

    removed redundant code

--- TRANSIT_TIME/all.py
+++ TRANSIT_TIME/all.py
@@ -17,28 +17,40 @@ def get_transmission_time(file_size, bandwidth):
     result = calculate_transmission_time(float(file_size), float(bandwidth))
     return float(result)
 
+def get_routing_delay(community, host_ip):
+    routing_delay_args = ["python", "routingDelay/", community, host_ip, "routingDelay/", str(time_interval)]
+    result = subprocess.run(routing_delay_args, capture_output=True, text=True)
+    return float(result.stdout.strip())
 
-def get_vals(file_size, host_ip):
+def get_vals(community, host_ip, file_size):
     with concurrent.futures.ThreadPoolExecutor() as executor:
         rtt_future = executor.submit(get_round_trip_time, host_ip)
         bandwidth_future = executor.submit(get_bandwidth, host_ip)
         transmission_time_future = executor.submit(get_transmission_time, file_size, bandwidth_future.result())
+        routing_delay_future = executor.submit(get_routing_delay, community, host_ip)
 
         round_trip_time = rtt_future.result()
         single_trip_time = round_trip_time / 2
+        print(f"Single-Trip Time: {single_trip_time:.4f}")
 
         transmission_time = transmission_time_future.result()
+        print(f"Transmission Time: {transmission_time}")
 
-        transit_time = single_trip_time + transmission_time 
-        print(f"{transit_time:.6f}")
+        #routing_delay = routing_delay_future.result()
+        #print(f"Routing Delay: {routing_delay}")
+        routing_delay = 0
 
-if len(sys.argv) != 4:
-    print("python transittime file_size time_interval host_ip")
+        transit_time = single_trip_time + transmission_time + routing_delay
+        print(f"Total Transit time : {transit_time}")
+
+if len(sys.argv) != 5:
+    print("python transittime community host_ip file_size time_interval")
     sys.exit(1)
 
-file_size = sys.argv[1] 
-time_interval = float(sys.argv[2]) 
-host_ip = sys.argv[3]
+community = sys.argv[1]
+host_ip = sys.argv[2] 
+file_size = sys.argv[3] 
+time_interval = float(sys.argv[4]) 
 
-get_vals( file_size, host_ip)
+get_vals(community, host_ip, file_size)
 
