4113643e936a486d7e178157fecd0467fe0b0c13
SANTHOSH17-DOT <root@localhost.localdomain>
2023-12-19 16:24:49 +0530

    Removed reduntant code

--- PACKET_INTERCEPTION/dummy_traffic.py
+++
@@ -1,4 +0,0 @@
-import requests
-while True:
-    requests.get("https://www.youtube.com")
-# https://theuselessweb.com/
--- PACKET_INTERCEPTION/main.py
+++ PACKET_INTERCEPTION/main.py
@@ -4,17 +4,7 @@ import netifaces
 import zstandard as zstd
 from dotenv import load_dotenv
 import json 
-import lz4.frame
-# import zlib
-# import os
 load_dotenv()
-
-# Important stuffs about zstandard package
-# write_content_size parameter improves performance
-# one-shot vs streaming modes
-# dictionaries
-# compression_params?
-# Multithreading
 
 def process_packet(packet):
     payload = packet.get_payload()
@@ -33,44 +23,29 @@ def process_packet(packet):
         
         source_ip = ip_header.src
         payload = bytes(scapy_packet[Raw])
-        # Outgoing packets(Compression)
-        if source_ip in host_ips:
+        if source_ip in host_ips: # Outgoing packets(Compression)
             print("Outgoing Traffic:")
-            # print(scapy_packet[TCP].summary())
-            # print(type(payload))
-            # with open('README.md','rb') as f:
-            #     payload = f.read()
-            # compressed_payload = cctx.compress(payload)
-            
+            start_time = time.time()
             compressed_payload = cctx.compress(payload)
+            end_time = time.time()
+            print("Compression time: ", end_time-start_time)
             # Consider UDP
             compressed_packet = IP(src=ip_header.src, dst=ip_header.dst) / TCP(sport=tcp_header.sport,dport=tcp_header.dport)
             compressed_packet = compressed_packet / compressed_payload
             print("Original Payload size:", len(payload))
             print("Compressed Payload size:", len(compressed_payload))
-            # compressed_packet.accept()
             packet.set_payload(bytes(compressed_packet))
         else: # Incoming packets(Decompression)
             print("Incoming Traffic:")
-            # print(scapy_packet[TCP].summary())
-            #print(payload)
-            # start_time = time.time()
+            start_time = time.time()
             decompressed_payload = dctx.decompress(payload, max_output_size=1048576)
-            # end_time = time.time()
-            # print(start_time-end_time)
+            end_time = time.time()
+            print("Decompression time: ", end_time-start_time)
             decompressed_packet = IP(src=ip_header.src, dst=ip_header.dst) / TCP(sport=tcp_header.sport,dport=tcp_header.dport)
-
             decompressed_packet = decompressed_packet / decompressed_payload
-
-            #decompressed_payload = zlib.decompress(payload)
             print("Original Payload size:", len(payload))
-            # print("Payloads length: ",len(payloads))
             print("Decompressed Payload size:", len(decompressed_payload))
-            # packet.set_payload(decompressed_payload)
-            # decompressed_packet.accept()
             packet.set_payload(bytes(decompressed_packet))
-        # print(scapy_packet.show())
-        # coflow schedule and compression
     packet.accept()
 
 interfaces = netifaces.interfaces()
@@ -81,23 +56,6 @@ for interface in interfaces:
         host_ips.append(addrs[netifaces.AF_INET][0]['addr'])
 
 QUEUE_NUM = int(os.environ.get("QUEUE_NUM"))
-
-samples = []
-for i in range(1,16):
-    with open(f'./captured_packets/captured_payloads{i}.json','rb') as f:
-        samples+=[f.read()]
-# files = [f for f in os.listdir('./github') if os.path.isfile(os.path.join('./github', f))]
-# for file_name in files:
-#         file_path = os.path.join('./github', file_name)
-#         with open(file_path, 'r') as file:
-#             data = file.read()
-#             samples.append(str(data))
-print(len(samples))
-# samples = [sample for sample in samples]
-# print(samples)
-dict_data = zstd.train_dictionary(275,samples)
-with open("dict_data", "w") as f:
-    f.write(str(dict_data.as_bytes()))
 
 cctx = zstd.ZstdCompressor(level=3,write_content_size=False)
 dctx = zstd.ZstdDecompressor()
